ea5831701ad2adb603349ef724b48d43dc926a34
Yi Wang <e@yi-wang.me>
2019-11-18 19:48:18 -0500

    chore: init fastxt-ios

---
+++ fastxt-ios/Fastxt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fastxt
+//
+//  Created by Yi Wang on 11/18/19.
+//  Copyright © 2019 Yi Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
