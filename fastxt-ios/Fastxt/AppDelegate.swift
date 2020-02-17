1ab9003c8713bd27f16dd81b891b80ece5a6307a
Yi Wang <e@yi-wang.me>
2020-02-17 17:29:44 -0500

    feat: add UIFileSharingEnabled

--- fastxt-ios/Fastxt/AppDelegate.swift
+++ fastxt-ios/Fastxt/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("good")
         let ft = RustFastxt()
         
         let txt = ft.run(json_input:"""
