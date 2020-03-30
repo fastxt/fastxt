e542fa698ca966100e2308913486893867ac47a5
Yi Wang <e@yi-wang.me>
2020-03-29 22:44:19 -0400

    feat: iOS add TxtRowView

---
+++ fastxt-ios/Fastxt/TxtRowView.swift
@@ -0,0 +1,21 @@
+//
+//  TxtRowView.swift
+//  Fastxt
+//
+//  Created by Yi Wang on 3/29/20.
+//  Copyright © 2020 Yi Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct TxtRowView: View {
+    var body: some View {
+        Text("TxtRowView")
+    }
+}
+
+struct TxtRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TxtRowView()
+    }
+}
