c9866a5e94bd2ff40698eb7ee11c02e1f61c7bc4
Yi Wang <e@yi-wang.me>
2020-03-29 23:13:18 -0400

    feat: add SearchBar

--- fastxt-ios/Fastxt/ContentView.swift
+++ fastxt-ios/Fastxt/ContentView.swift
@@ -9,18 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText : String = ""
+
     var body: some View {
-        List {
-            TxtRowView()
-            TxtRowView()
-            TxtRowView()
+        NavigationView {
+            VStack {
+                SearchBar(text: $searchText, placeholder: "type to search")
+                List {
+                    TxtRowView()
+                    TxtRowView()
+                }.navigationBarTitle(Text("Fastxt"))
+            }
         }
-        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+    var placeholder: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.placeholder = placeholder
+        searchBar.searchBarStyle = .minimal
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
     }
 }
